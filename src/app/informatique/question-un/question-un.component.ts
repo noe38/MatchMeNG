ace2d5f10ea6953e0ee142d6ab33b3f05897e7b2
noemie graignic <noemie.graignic@free.fr>
2019-11-28 23:18:49 +0100

    Ajout d'images illustrant les questions - informatique

--- src/app/informatique/question-un/question-un.component.ts
+++ src/app/informatique/question-un/question-un.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -17,13 +17,14 @@ export class QuestionUnComponent implements OnInit {
     this.initForm();
   }
 
-  initForm(){
+  initForm() {
     this.questionsForm = this.formBuilder.group({
-
+      
     })
   }
 
-  onReponsesInformatique(){
+  onReponsesInformatique() {
+    console.log('réponse de la question 1 :' + this.questionsForm.value);
     this.router.navigate(['/informatique', 'question2']);
   }
 }
@@ -32,31 +33,31 @@ export class QuestionUnComponent implements OnInit {
 
 
 
- /*  constructor(private formBuilder: FormBuilder,
-    private diversService: DiversService,
-    private router: Router) { }
+/*  constructor(private formBuilder: FormBuilder,
+   private diversService: DiversService,
+   private router: Router) { }
 
-  ngOnInit() {
-    this.initForm();
-  }
+ ngOnInit() {
+   this.initForm();
+ }
 
-  initForm() {
-    this.diversForm = this.formBuilder.group({
-      title: ['', Validators.required],
-      date: ['', Validators.required],
-      synopsis:''
-    });
-  }
+ initForm() {
+   this.diversForm = this.formBuilder.group({
+     title: ['', Validators.required],
+     date: ['', Validators.required],
+     synopsis:''
+   });
+ }
 
-  onSaveDivers() {
-    const title = this.diversForm.get('title').value;
-    const date = this.diversForm.get('date').value;
-    const synopsis = this.diversForm.get('synopsis').value;
-    const newDivers = new Divers(title, date);
-    newDivers.synopsis = synopsis;
-    if (this.fileUrl && this.fileUrl !== '') {
-      newDivers.photo = this.fileUrl;
-    }
-    this.diversService.createNewDivers(newDivers);
-    this.router.navigate(['/diversification']);
-  } */
+ onSaveDivers() {
+   const title = this.diversForm.get('title').value;
+   const date = this.diversForm.get('date').value;
+   const synopsis = this.diversForm.get('synopsis').value;
+   const newDivers = new Divers(title, date);
+   newDivers.synopsis = synopsis;
+   if (this.fileUrl && this.fileUrl !== '') {
+     newDivers.photo = this.fileUrl;
+   }
+   this.diversService.createNewDivers(newDivers);
+   this.router.navigate(['/diversification']);
+ } */
