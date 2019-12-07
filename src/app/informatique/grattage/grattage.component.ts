86da34b716db481f5d766cab9ed77b2d25b582ed
noemie graignic <noemie.graignic@free.fr>
2019-12-07 16:16:27 +0100

    mise ne place des div perdu/gagné avec condition non relie au service

--- src/app/informatique/grattage/grattage.component.ts
+++ src/app/informatique/grattage/grattage.component.ts
@@ -9,6 +9,7 @@ export class GrattageComponent implements OnInit {
   @ViewChild('zonegrattage', { static: true }) canvas: ElementRef<HTMLCanvasElement>;
   private context: CanvasRenderingContext2D;
   imgrattage1: any = "assets/img/informatique/grattez-ici.png";
+  isRepTrue: boolean;
 
 
 
@@ -16,22 +17,31 @@ export class GrattageComponent implements OnInit {
 
   ngOnInit() {
     this.context = this.canvas.nativeElement.getContext('2d');
+    this.isRepTrue = true;
 
     // J'ai essayé de mettre une image à gratter mais elle ne s'affiche pas alors j'ai dessiné
     //this.context.globalCompositeOperation = 'destination-over';
-    const imgrattage = new Image();
+    //const imgrattage = new Image();
     //imgrattage.addEventListener('load', function(){this.context.drawImage(imgrattage, 0, 0, 300, 300)});
     // imgrattage.src = "assets/img/informatique/grattez-ici.png";
-    imgrattage.src = "assets/img/informatique/grattez-ici.png";
-    this.context.drawImage(imgrattage, 0, 0, 200, 200);
+    //imgrattage.src = "assets/img/informatique/grattez-ici.png";
+    //this.context.drawImage(imgrattage, 0, 0, 250, 250);
 
+    // dessin du canvas avec marqué Grattez ici
     this.context.fillStyle = 'rgba(23, 145, 167, 1)';
     this.context.fillRect(30, 30, 390, 290);
     this.context.font = "bold 44pt Calibri,Geneva,Arial";
     this.context.fillStyle = "rgb(59, 206, 206)";
     this.context.fillText("Grattez ici", 100, 190);
 
+  
   }
 
+  activergrattage(){
+    this.mousedown();
+  }
 
+  mousedown(){
+
+  }
 }
